7128e087fff260d542c5407d75942766366e4f3c
paulrich1973 <121955706+paulrich1973@users.noreply.github.com>
2023-05-16 22:58:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ async function getChatGPTResponse(userMessage) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer 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', // Replace with your actual API key
+      'Authorization': 'Bearer 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', // Replace with your actual API key
     },
     body: JSON.stringify({
       'messages': [{'role': 'system', 'content': 'You are a user'}, {'role': 'user', 'content': userMessage}],
